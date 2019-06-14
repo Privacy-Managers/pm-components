8e14ed43c26554cb53acc07075fd95fe7bd8284a
Manvel Saroyan <saroyanm@gmail.com>
2019-06-14 14:12:15 +0400

    Added toggle test for puppeteer

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,16 +1,16 @@
 describe("Testing settings-list", () =>
 {
-  it("Doesn't do much", () => {
+  it("Navigate to the component page", () => {
     cy.visit("http://127.0.0.1:3000/setting-list/setting-list.html");
   });
-  it("smth", () => {
+  it("Should change 'checked' attribute on toggle() method call", () => {
     cy.window().then((win) => {
         const settingList = win.document.querySelector("setting-list");
         settingList.toggle();
         expect(settingList.hasAttribute("checked")).to.equal(true);
       });
     });
-  it("select", () => {
+  it("Focus on button", () => {
     focusInShadow("setting-list", "button").then(() =>
     {
       
--- puppeteer/index.js
+++ puppeteer/index.js
@@ -4,25 +4,40 @@ const assert = require('assert');
 
 let browser;
 let page;
+let settingListHandle;
 
 before(async () => {
   browser = await puppeteer.launch({headless: false});
   page = await browser.newPage()
-})
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
+  const pathToComponent = resolve("components/setting-list/setting-list.html");
+  await page.goto(`file://${pathToComponent}`);
+  settingListHandle = await page.$('setting-list');
+});
 
-describe("Testing settings-list", () =>
+describe("Testing settings-list component", () =>
 {
-  it("smth", async () =>
+  it("Should change 'checked' attribute on toggle() method call", async () =>
   {
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
-    const pathToComponent = resolve("components/setting-list/setting-list.html");
-    await page.goto(`file://${pathToComponent}`);
-    const settingListHandle = await page.$('setting-list');
-    const isChecked = await page.evaluate((settingListElem) => {
-      settingListElem.toggle();
-      return settingListElem.hasAttribute("checked");
-    }, settingListHandle);
-    assert.equal(isChecked, true);
+    function toggleAndGetChecked()
+    {
+      return page.evaluate((settingListElem) => {
+        settingListElem.toggle();
+        return settingListElem.getAttribute("checked");
+      }, settingListHandle);
+    }
+
+    function isEnabled()
+    {
+      return page.evaluate((settingListElem) => {
+        return settingListElem.isEnabled();
+      }, settingListHandle);
+    }
+
+    assert.equal(await toggleAndGetChecked(), "true");
+    assert.equal(await isEnabled(), true);
+    assert.equal(await toggleAndGetChecked(), "false");
+    assert.equal(await isEnabled(), false);
   });
 });
 

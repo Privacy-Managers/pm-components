26b8d9db012e345fb2f197cf9e95334f945202b6
Manvel Saroyan <saroyanm@gmail.com>
2019-07-16 00:12:37 +0500

    Added addSubItem() test

--- tests/puppeteer/table-list.js
+++ tests/puppeteer/table-list.js
@@ -16,19 +16,32 @@ before(async () =>
   tableListHandle = await page.$('table-list');
 });
 
-function populateItemsGetLoadedLength()
+function addItems(objItems)
 {
-  return page.evaluate((tableListHandle) => {
-    const objItems = [];
-    for (let i = 0; i < 300; i++) {
-      objItems.push({
-        dataset:  { access: `example${i}.com`},
-        texts: {"domain": `example${i}.com`, "cookienum": "3 Cookies"}
-      });
+  return page.evaluate((tableListHandle, objItems) =>
+  {
+    return tableListHandle.addItems(objItems);
+  }, tableListHandle, objItems);
+}
+
+function getLoadedAmount(accessor)
+{
+  return page.evaluate((tableListHandle, accessor) => {
+    let elements = tableListHandle.shadowRoot.querySelector("ul").children;
+    if (accessor)
+    {
+      const {index} = tableListHandle.indexOfAccessor(accessor);
+      elements = elements[index].querySelector("ul").children;
     }
-    tableListHandle.addItems(objItems);
-    return tableListHandle.shadowRoot.querySelector("ul").children.length;
-  }, tableListHandle);
+    return elements.length;
+  }, tableListHandle, accessor);
+}
+
+function addSubItem(objItem, accessor)
+{
+  return page.evaluate((tableListHandle, objItem, accessor) => {
+    return tableListHandle.addSubItem(objItem, accessor);
+  }, tableListHandle, objItem, accessor);
 }
 
 function indexOfAccessor(accessor)
@@ -49,7 +62,16 @@ describe("Table-list component", () =>
 {
   it("Populating Table with 300 items should load first 50 items by default", async() =>
   {
-    const loaded =  await populateItemsGetLoadedLength();
+    const objItems = [];
+    for (let i = 0; i < 300; i++) {
+      objItems.push({
+        dataset:  { access: `example${i}.com`},
+        texts: {"domain": `example${i}.com`, "cookienum": "3 Cookies"}
+      });
+    }
+
+    await addItems(objItems);
+    const loaded =  await getLoadedAmount();
     assert.equal(loaded, 50);
   });
   it("indexOfAccessor() method should return index for accessor", async() =>
@@ -64,6 +86,17 @@ describe("Table-list component", () =>
     const {index, subIndex} = await indexOfAccessor("example1.com");
     assert.equal(index, -1);
     assert.equal(subIndex, -1);
+  });
+
+  it("addSubItem() method should add subitems to the item with specified accessor", async() =>
+  {
+    for (let i = 0; i < 5; i++) {
+      addSubItem({
+        dataset:  { access: `subexample.com${i}`},
+          texts: {"name": `subexample.com${i}`, "value": "3 Cookies"}
+      }, "example0.com");
+    }
+    assert.equal(await getLoadedAmount("example0.com"), 5);
   });
 });
 

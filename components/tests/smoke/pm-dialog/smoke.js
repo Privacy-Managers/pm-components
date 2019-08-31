9cc5b4bddecd04f4c874d3dfdbb011157f68c37c
Manvel Saroyan <saroyanm@gmail.com>
2019-08-31 20:03:45 +0500

    Added action listeners to the pm-dialog

--- components/tests/smoke/pm-dialog/smoke.js
+++ components/tests/smoke/pm-dialog/smoke.js
@@ -1,6 +1,31 @@
 
 document.addEventListener("DOMContentLoaded", () =>
 {
-  document.querySelector("pm-dialog").setData({title: "My Title", fields: {description: "My description"}});
-  document.querySelector("pm-dialog").showDialog();
+  document.querySelectorAll("button").forEach((button) => 
+  {
+    button.addEventListener("click", buttonClickHandler);
+  });
 });
+
+function buttonClickHandler(ev)
+{
+  const pmDialog = document.querySelector("pm-dialog");
+  switch (ev.target.id) {
+    case "basic":
+      const data = {
+        title: "My Title", 
+        fields: {
+          description: "My description"
+        }
+      };
+      pmDialog.setData(data);
+      pmDialog.showDialog();
+      break;
+    case "advanced":
+
+      break;
+    default:
+      break;
+  }
+  
+}

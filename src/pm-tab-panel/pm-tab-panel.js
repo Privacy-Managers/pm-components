95abd8a299f209ce106c09a0e3318c6cb03ded35
Manvel Saroyan <saroyanm@gmail.com>
2019-09-12 16:27:38 +0300

    #4 - Added navigations for tab component

--- src/pm-tab-panel/pm-tab-panel.js
+++ src/pm-tab-panel/pm-tab-panel.js
@@ -2,14 +2,22 @@ class PmTabPanel extends HTMLElement {
   constructor() {
     super();
     this.tabs = [];
+    this.selectedTab = null;
   }
-
   connectedCallback()
   {
-    this.tabs = this.querySelectorAll("pm-tab");
+    this.tabs = [...this.querySelectorAll("pm-tab")];
     this.addEventListener("click", (e) =>
     {
       this.select(e.target.id);
+    });
+
+    this.addEventListener("keydown", ({key}) =>
+    {
+      if (key === "ArrowUp" || key === "ArrowRight")
+        this.selectNextTab();
+      if (key === "ArrowDown" || key === "ArrowLeft")
+        this.selectPrevTab();
     });
   }
 
@@ -19,14 +27,33 @@ class PmTabPanel extends HTMLElement {
     {
       tab._hide();
       if (tab.id === id)
+      {
         tab._show();
+        this.selectedTab = tab;
+      }
     });
+  }
+
+  _getSelectedTabIndex()
+  {
+    return this.tabs.indexOf(this.selectedTab);
+  }
+
+  selectNextTab()
+  {
+    const index = this._getSelectedTabIndex() + 1;
+    this.select(this.tabs[index >= this.tabs.length ? 0 : index].id);
+  }
+  selectPrevTab()
+  {
+    const index = this._getSelectedTabIndex() - 1;
+    this.select(this.tabs[index >= 0 ? index : this.tabs.length - 1].id);
   }
 }
 
 class PmTabs extends HTMLElement {
   constructor() {
-    super();
+    super(); 
   }
 }
 
@@ -45,12 +72,15 @@ class PmTab extends HTMLElement {
   _show()
   {
     this.setAttribute("aria-selected", true);
+    this.setAttribute("tabindex", "0");
+    this.focus();
     this.panel._show();
   }
 
   _hide()
   {
     this.removeAttribute("aria-selected");
+    this.setAttribute("tabindex", "-1");
     this.panel._hide();
   }
 }

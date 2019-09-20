662ec599984ff437a11b55590a6c9c18d0a5be41
Manvel Saroyan <saroyanm@gmail.com>
2019-09-20 19:39:11 +0400

    #29 - Separated CSS and HTML into a separate file as a build step

---
+++ build/index.js
@@ -0,0 +1,53 @@
+const {readFile, readdir, writeFile, mkdir, copyFile} = require("fs").promises;
+const {existsSync} = require("fs");
+const {join} = require("path");
+
+const inputDir = "components/src";
+const outputDir = "components/dist";
+const utils = join(inputDir, "utils.js");
+
+async function run()
+{
+  const components = await getComponents();
+  await copyFile(utils, join(outputDir, "utils.js"));
+  components.forEach(build);
+  console.log("done");
+}
+
+
+async function getComponents()
+{
+  
+  const dirents = await readdir(inputDir, { withFileTypes: true });
+  const components = dirents.filter(dirent => dirent.isDirectory())
+                            .map(dirent => dirent.name);
+  return components;
+}
+
+async function readContent(path)
+{
+  if(existsSync(path))
+    return await readFile(path, "utf8");
+  else
+    return "";
+}
+
+async function ensureDir(dir)
+{
+  if (!existsSync(dir))
+    await mkdir(dir);
+}
+
+async function build(name)
+{
+  const componentContent = await readContent(join(inputDir, name, name + ".js"));
+  const css = await readContent(join(inputDir, name, name + ".css"));
+  const html = await readContent(join(inputDir, name, name + ".html"));
+  const builtContent = componentContent.replace("${pmLoadCSS}", css).replace("${pmLoadHTML}", html);
+  await ensureDir(outputDir);
+  await ensureDir(join(outputDir, name));
+  await writeFile(join(outputDir, name, name + ".js"), builtContent, "utf8");
+  console.log(`${name} build ready`);
+}
+
+run();
--- components/src/pm-button/pm-button.js
+++ components/src/pm-button/pm-button.js
@@ -5,37 +5,9 @@ class PmButton extends HTMLElement {
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
       <style>
-        :host
-        {
-          --color-primary: #679c16;
-          --color-secondary: #989898;
-        }
-        button
-        {
-          cursor: pointer;
-          line-height: 12px;
-          color: #fff;
-          font-weight: bold;
-          border-radius: 2px;
-          display: inline-block;
-          width: auto;
-          margin: 5px;
-          padding: 7px;
-          background-color: var(--color-primary);
-          border: solid 1px transparent;
-        }
-
-        button:hover
-        {
-          box-shadow: 0px 0px 1px #000;
-        }
-
-        button:after
-        {
-          margin-left: 5px;
-        }
+        ${pmLoadCSS}
       </style>
-      <button type="button"><slot></slot></button>
+      ${pmLoadHTML}
     `;
   }
 }

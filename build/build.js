ade05a7562da3d094328e7534c427da1ff77be13
Manvel Saroyan <saroyanm@gmail.com>
2019-09-21 01:00:44 +0400

    #31 - Watch and run build when on dev server

---
+++ build/build.js
@@ -0,0 +1,52 @@
+const {readFile, readdir, writeFile, mkdir, copyFile} = require("fs").promises;
+const {existsSync} = require("fs");
+const {join} = require("path");
+
+const inputDir = "./components/src";
+const outputDir = "./components/dist";
+const utils = join(inputDir, "utils.js");
+
+async function build()
+{
+  const components = await getComponents();
+  await ensureDir(outputDir);
+  await copyFile(utils, join(outputDir, "utils.js"));
+  components.forEach(compile);
+}
+
+
+async function getComponents()
+{
+  
+  const dirents = await readdir(inputDir, { withFileTypes: true });
+  const components = dirents.filter(dirent => dirent.isDirectory())
+                            .map(dirent => dirent.name);
+  return components;
+}
+
+async function readContent(path)
+{
+  if(existsSync(path))
+    return await readFile(path, "utf8");
+  else
+    return "";
+}
+
+async function ensureDir(dir)
+{
+  if (!existsSync(dir))
+    await mkdir(dir);
+}
+
+async function compile(name)
+{
+  const componentContent = await readContent(join(inputDir, name, name + ".js"));
+  const css = await readContent(join(inputDir, name, name + ".css"));
+  const html = await readContent(join(inputDir, name, name + ".html"));
+  const builtContent = componentContent.replace("${pmLoadCSS}", css).replace("${pmLoadHTML}", html);
+  await ensureDir(join(outputDir, name));
+  await writeFile(join(outputDir, name, name + ".js"), builtContent, "utf8");
+  console.log(`${name} build ready.`);
+}
+
+module.exports = {build};

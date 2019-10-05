35d8e3aac811fd4fc5a62f19232eee6b5aa36832
Manvel Saroyan <saroyanm@gmail.com>
2019-10-05 14:04:46 +0400

    Updated bin script

--- bin/pm-components.js
+++ bin/pm-components.js
@@ -2,6 +2,14 @@
 
 "use strict";
 
+const config = require('../webpack.config.js');
 const webpack = require('webpack');
+const compiler = webpack(config);
 
-webpack(require("../webpack.config"));
+compiler.run((err, res) =>
+{
+  if (err)
+    console.error("Error", err);
+  else
+    console.log("Components are ready");
+});
